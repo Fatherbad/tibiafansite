5b96492ac1f5973f27f6e3e8f32f6a3ad407a3c8
Stephen <stephen.d.motherwell@gmail.com>
2020-07-28 09:59:22 -0700

    node project setup with a landing page

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// external modules
+
+const express = require("express");
+const path = require("path");
+const {Client} = require("pg");
+
+const app = express();
+const port = process.env.PORT || "4000";
+
+
+//configs
+app.use(express.static(__dirname + '/views/'));
+
+//routes
+
+app.get("/", (req,res) => {
+	res.sendFile(views/index.html);
+});
+
+
+//on activation
+
+console.log(port);
+app.listen(port, () => {
+	console.log('listening on port:');
+});
